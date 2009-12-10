91ab8ffaeea70b503163d1e982141f5e4cba9749
Clark Gaebel <cg.wowus.cg@gmail.com>
2009-12-09 20:34:23 -0500

    I no longer have a use for release.bat. Therefore, removed.

--- Release/Release.bat
+++
@@ -1,5 +0,0 @@
-@upx.exe -9 --ultra-brute NOP.dll
-@copy NOP.dll "C:\Users\Clark Gaebel\Desktop\NOP - Release\NOP.dll"
-@"C:\Users\Clark Gaebel\Desktop\ExeCryptor\EXECrypt.exe" "C:\Users\Clark Gaebel\Desktop\ExeCryptor\Projects\NOP.ep2"
-@copy "C:\Users\Clark Gaebel\Desktop\NOP - Release\NOP.dll" "C:\Program Files (x86)\Freestyle GunZ\NOP.dll"
-@pause
