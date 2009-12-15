079e683be0f4f76f3747d6bb5e467c879f50055c
Clark Gaebel <cg.wowus.cg@gmail.com>
2009-12-15 18:08:59 -0500

    Removed targetver.h. It's practically useless.

--- stdafx.h
+++ stdafx.h
@@ -5,7 +5,24 @@
 
 #pragma once
 
-#include "targetver.h"
+// The following macros define the minimum required platform.  The minimum required platform
+// is the earliest version of Windows, Internet Explorer etc. that has the necessary features to run 
+// your application.  The macros work by enabling all features available on platform versions up to and 
+// including the version specified.
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER					// Specifies that the minimum required platform is Windows XP.
+#define WINVER 0x0501           // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows XP.
+#define _WIN32_WINNT 0x0501     // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
+#define _WIN32_WINDOWS 0x0410	// Change this to the appropriate value to target Windows Me or later.
+#endif
 
 // STL Containers.
 #include <vector>
@@ -20,8 +37,13 @@
 
 // Platform-specific.
 #include <windows.h>
+#include <dbghelp.h>
 #include <shlwapi.h>
 #pragma comment(lib, "shlwapi.lib")
+
+// Standard C.
+#include <stddef.h>
+#include <strsafe.h>
 
 // Standard C++.
 #include <cassert>
@@ -37,6 +59,7 @@
 // Application-specific.
 #include "defs.h"
 #include "IHash.h"
+#include "Detours/detours.h"
 #include "Detours/CDetour.h"
 #include "NOP.h"
 #include "Core.h"
