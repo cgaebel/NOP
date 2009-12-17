4449fca2b10fb9527a497c987dbaea0ce99cd9fa
Clark Gaebel <cg.wowus.cg@gmail.com>
2009-12-16 21:33:44 -0500

    Shrunk down PointerIsInModule predicate.

--- HideFromPEB.cpp
+++ HideFromPEB.cpp
@@ -8,18 +8,12 @@ static TEB* GetTEB()
 }
 
 // check if the "signature" variable is inside this module, if so it is our module
-static bool PointerIsInModule(const void* pointer, const LDR_MODULE* module)
+static bool PointerIsInModule(DWORD pointer, const LDR_MODULE* module)
 {
-	return	(
-				(DWORD)pointer > (DWORD)(module->BaseAddress)
-			)
-			&&
-			(
-				(DWORD)pointer <
-				(
-					(DWORD)(module->BaseAddress) + (DWORD)(module->SizeOfImage)
-				)
-			);
+	DWORD startAddress = (DWORD)module->BaseAddress;
+	DWORD endAddress = startAddress + (DWORD)module->SizeOfImage;
+
+	return (pointer > startAddress) && (pointer < endAddress);
 }
 
 static void RemoveFromRootList(LIST_ENTRY* toRemove)
@@ -31,7 +25,7 @@ static void RemoveFromRootList(LIST_ENTRY* toRemove)
 static void RemoveThisModuleFromList(LIST_ENTRY* listHead)
 {
 	// Can point to any string as long as it's in OUR address space.
-	static const char* signature = __DATE__;
+	static DWORD signature = (DWORD)__DATE__;
 
 	for(LIST_ENTRY* currentEntry = listHead->Flink; currentEntry != listHead; currentEntry = currentEntry->Flink)
         if(PointerIsInModule(signature, CONTAINING_RECORD(currentEntry, LDR_MODULE, InMemoryOrderModuleList)))
